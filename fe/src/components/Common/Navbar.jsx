5e98026a43f7c644dd4a360e0ddc69b397d632c3
Kanghocit <Ankhangit06@gmail.com>
2025-02-17 00:44:27 +0700

    side menu

--- fe/src/components/Common/Navbar.jsx
+++ fe/src/components/Common/Navbar.jsx
@@ -7,8 +7,13 @@ import {
 } from "react-icons/hi2";
 import Searchbar from "./Searchbar";
 import CartDrawer from "../Layout/CartDrawer";
+import { IoMdClose } from "react-icons/io";
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+  const toggleNavCartDrawer = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
   const toggleCartDrawer = () => {
     setDrawerOpen(!drawerOpen);
   };
@@ -53,7 +58,10 @@ const Navbar = () => {
           <Link to={"/profile"} className="hover:text-black">
             <HiOutlineUser className="h-6 w-6 text-gray-700" />
           </Link>
-          <button onClick={toggleCartDrawer} className="relative hover:text-black">
+          <button
+            onClick={toggleCartDrawer}
+            className="relative hover:text-black"
+          >
             <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
             <span className="absolute bg-kang-red -top-1  text-white text-xs rounded-full px-2 py-0.5">
               4
@@ -61,12 +69,58 @@ const Navbar = () => {
           </button>
           {/* Search */}
           <Searchbar />
-          <button className="md:hidden">
+          <button onClick={toggleNavCartDrawer} className="md:hidden">
             <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
           </button>
         </div>
       </nav>
-      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+
+      {/* Mobile Navigation */}
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawerOpen ? "transition-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={toggleNavCartDrawer}>
+            <IoMdClose className="w-6 h-6 text-gray-600" />
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Menu</h2>
+          <nav className="space-y-4">
+            <Link
+              to={"#"}
+              onClick={toggleNavCartDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Men
+            </Link>
+            <Link
+              to={"#"}
+              onClick={toggleNavCartDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Woman
+            </Link>
+            <Link
+              to={"#"}
+              onClick={toggleNavCartDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Top Wear
+            </Link>
+            <Link
+              to={"#"}
+              onClick={toggleNavCartDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Bottom Wear
+            </Link>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
