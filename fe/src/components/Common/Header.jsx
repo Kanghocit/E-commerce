9fb944fc06782ca40334608a273f7dd0ae2001f2
Kanghocit <Ankhangit06@gmail.com>
2025-02-16 15:26:33 +0700

    create-repo

---
+++ fe/src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopBar from "../Layout/TopBar";
+
+const Header = () => {
+  return (
+    <>
+      {/* Topbar */}
+      <TopBar />
+      {/* Navbar */}
+      {/* Cart */}
+    </>
+  );
+};
+
+export default Header;
