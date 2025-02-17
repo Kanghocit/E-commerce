e73d36caec507688a2df851af8db80adfcd78609
Kanghocit <Ankhangit06@gmail.com>
2025-02-18 00:46:07 +0700

    footer

--- fe/src/components/Layout/UserLayout.jsx
+++ fe/src/components/Layout/UserLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 
 const UserLayout = () => {
   return (
@@ -7,7 +8,14 @@ const UserLayout = () => {
       {/* Header */}
       <Header />
       {/* Main content */}
+      <main>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        corrupti sapiente necessitatibus vel consequatur magnam, reiciendis,
+        error hic, odio eligendi culpa? Doloremque debitis, nam quia natus
+        quaerat quo ab cupiditate.
+      </main>
       {/* Footer */}
+      <Footer />
     </>
   );
 };
